1c14b00d720f55159daaf2b939a0774e402b4bb9
TamayoChristian <105449824+TamayoChristian@users.noreply.github.com>
2022-06-23 12:59:00 -0500

    Add files via upload

---
+++ Tareas_2/Funciones2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+1. Una función que recibe como parametros 2 número enteros, que indica el inicio y el fin de una serie.
+Se pide calcular la sumatoria de los números comprendidos entre el inicio y fin de la serie.
+Nota: El primer entero puede ser mayor o menor que el segundo.
+
+*/
+
+/*
+2. Una función que recibe 2 parametros que indican el total de filas y columnas que se deben emplear para 
+imprimir un rectangulo por pantalla.
+Ejemplo: Rectangulo (2,5)=
+*****
+*****
+*/
+
+/*
+3. Usando la función factorial vista en clases, se pide que desde el main se imprima la sumatoria de los factoriales 
+desde 1 a 15 (debe utilizar un bucle)
+*/
+
+/*
+4. Diseñe la función que genere un número aleatorio. La función debe devolver un número aleatorio entre 1 y 6 
+(invesitgar como se generan números aleatorios)
+*/
+
+int pedro (int f)
+{
+  int a;
+  int b;
+  if(a>b)
+  {
+    for (int i=1; b>=a; i++)
+    {
+        b + i = i
+    }
+  }
+  else 
+  {
+     for (int i=1; a>=b; i++)
+    {
+        a + i = i
+    }
+  }
+}
+
+int main()
+{
+
+
+    return 0;
+}
